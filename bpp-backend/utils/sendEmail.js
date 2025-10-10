889cf95eed71bee812488b75b086f0b4aea0c215
prosperinabosqueprotector-a11y <prosperinabosqueprotector@gmail.com>
2025-10-10 16:19:48 -0500

    Update sendEmail.js changing to Resend

--- bpp-backend/utils/sendEmail.js
+++ bpp-backend/utils/sendEmail.js
@@ -1,31 +1,22 @@
-//const dotenv = require("dotenv");
-var nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
-//dotenv.config();
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, 
-  //service: "gmail", // o tu servicio SMTP
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, text }) => {
   try {
-    await transporter.sendMail({
-      from: `<${process.env.EMAIL_USER}>`,
+    console.log("Intentando enviar correo vía Resend...");
+
+    await resend.emails.send({
+      from: process.env.EMAIL_FROM,
       to,
       subject,
       text,
     });
+
     console.log("Correo enviado a:", to);
   } catch (err) {
-    console.error("Error al enviar correo:", err.message, err.code);
     console.error("Error al enviar correo:", err);
+    throw err;
   }
 };
 
